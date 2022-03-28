d9a2f0ae1aa4929ac828278c1fa5c50fe55ce8c4
João Rodrigo de Oliveira Ziegler <jr.ziegler@gmail.com>
2022-03-28 20:09:32 +0200

    add controller and repository in the IoC

---
+++ Discount.API/Controllers/DiscounterController.cs
@@ -0,0 +1,38 @@
+using Discount.API.Entities;
+using Discount.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Discount.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class DiscounterController : ControllerBase
+    {
+        private readonly IDiscountRepository _repository;
+
+        public DiscounterController(IDiscountRepository repository)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+
+        [HttpGet("{productName}", Name = "GetDiscount")]
+        public async Task<ActionResult<Coupon>> GetDiscount(string productName)
+        {
+            var coupon = await _repository.GetDiscount(productName);
+            return Ok(coupon);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Coupon>> CreateDiscount([FromBody] Coupon coupon)
+        {
+            await _repository.CreateDiscount(coupon);
+            return CreatedAtRoute("GetDiscount", new { productName = coupon.ProductName }, coupon);
+        }
+
+        [HttpDelete("{productName", Name = "DeleteDiscount")]
+        public async Task<ActionResult<bool>> DeleteDsicount(string productName)
+        {
+            return Ok(await _repository.DeleteDiscount(productName));
+        }
+    }
+}
--- Discount.API/Program.cs
+++ Discount.API/Program.cs
@@ -1,9 +1,10 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Discount.API.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
